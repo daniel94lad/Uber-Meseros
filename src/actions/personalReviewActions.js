ca5c59e8bc0877f22df530f2305e21a6685905ad
Daniel Lopez <daniel94lad@gmail.com>
2019-10-26 11:14:23 -0500

    'LoginRodrigoParo'

--- src/actions/personalReviewActions.js
+++ src/actions/personalReviewActions.js
@@ -1,15 +1,17 @@
 import {TRAER_TODO,CARGANDO,ERROR} from '../types/personalReviewTypes';
+import axios from 'axios';
 
 export const traerTodo =()=> async (dispatch)=>{
     dispatch({
         type:CARGANDO
     });
     try{
-        
+        const users = await axios.get('http://localhost:8000/profiles/')
         dispatch({
             type:TRAER_TODO,
-            payload:'Se logro'
+            payload:users.data
         })
+        console.log(users)
 
     }catch(error){
         console.log(error.message);
--- src/components/PersonalReview/index.js
+++ src/components/PersonalReview/index.js
@@ -4,9 +4,51 @@ import * as PersonalReviewActions from '../../actions/personalReviewActions';
 import Content from './Content';
 import Spinner from '../General/Spinner';
 import Fatal from '../General/Fatal';
+import axios from 'axios';
 
 
 class personalReview extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username:"",
+            password:"",
+        }
+    }
+    _onSubmit = async (e)=>{
+        
+        e.preventDefault()
+        const {username,password} = this.state
+        try{
+            
+            let request = await axios.post('http://localhost:8000/api/login/',{
+                username,
+                password
+            })
+
+        const {data} = request
+        localStorage.setItem('token',data.token)
+        window.location.replace('http://localhost:3000')
+        
+
+        }catch(error){
+            console.log(error)
+            console.log(this.state)
+            alert("Tu usuario o contrasena son incorrectos, por favor revisalos")
+        }
+    }
+    handleInputChange=(event)=>{
+        this.setState({
+            [event.target.name]: event.target.value
+          })
+          
+          
+    }
+    componentDidMount(){
+        if(!Object.keys(this.props.users).length){
+            this.props.traerTodo()
+        }
+    }
     ponerContenido = ()=>
     {
         if(this.props.cargando){
@@ -21,9 +63,38 @@ class personalReview extends Component{
     render(){
         return (
             <div>
-                
-                {this.ponerContenido()}
-            </div>
+            <div id="login__content">
+                        <img src="https://i2.wp.com/thehappening.com/wp-content/uploads/2017/06/portada-55.jpg?fit=1024%2C694&ssl=1" alt="Login" />
+                        
+                        <div className="login__center">
+                            <h2>Inicio de Sesión</h2>
+                            <p><strong>¡Bienvenido!</strong> Es momentos de unas carnitas <span role="img" aria-label="sheep"> &#128536;</span></p>
+
+                            <form  onSubmit={this._onSubmit}>
+                                <label><strong> Usuario:</strong></label>
+
+                            <input type="text"
+                            placeholder="Nombre de Usuario"
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.handleInputChange}
+                            required
+                        />
+                        <label><strong>Contraseña:</strong></label>
+                    <input type="password" 
+                    placeholder="Contraseña"
+                    value={this.state.password}
+                    name="password"
+                    onChange={this.handleInputChange}
+                    required
+                    />
+                    
+                    
+                            <input id="btn"  type="submit" value="Enviar"/>
+                            </form>
+                    </div>
+        </div>
+    </div>
         )
     }
 }
